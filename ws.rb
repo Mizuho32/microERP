ed44c45c49589279c4c12378bbe1fe12bd4d404a
Mizuho <mizuho128@gmail.com>
2020-05-05 13:58:35 +0900

    音声入力最低限実装

--- ws.rb
+++ ws.rb
@@ -5,6 +5,7 @@ require 'sinatra'
 
 set :bind, '0.0.0.0'
 
+config       = YAML.load_file(ARGV.first)
 types_file   = 'types.yaml';
 parents_file = 'parents.yaml';
 data = {
@@ -31,8 +32,19 @@ trap(:INT) {
   Sinatra::Base.quit!
 }
 
+get "/main.js" do
+  content_type "text/javascript"
+  erb "main.js".to_sym, locals: {
+    lang: config[:client][:lang],
+    autostart: config[:client][:autostart],
+    form_names: config[:client][:form_names],
+    commands: config[:client][:commands]
+  }
+end
+
 get ?/ do
   erb "index.htm".to_sym, locals: {
+    autostart: config[:client][:autostart],
     types: types, parents: parents,
     type: data[:type].to_s, parent: data[:parent].to_s
   }
@@ -45,6 +57,9 @@ post ?/ do
   parent  = params["parent"] 
   comment = params["comment"]
 
+if params.key? "save" then
+  csv.flush
+else
 
   if not type.nil? and not types.include?(type)then
     types << type
@@ -56,7 +71,7 @@ post ?/ do
     File.write(parents_file, parents.to_yaml)
   end
 
-  if type == "収納" then
+  if type == config[:server][:type_storage] then
     parents << name
     File.write(parents_file, parents.to_yaml)
   end
@@ -65,8 +80,10 @@ post ?/ do
 
   data[:type]   = type||data[:type]
   data[:parent] = parent||data[:parent]
+end
 
   erb "index.htm".to_sym, locals: {
+    autostart: config[:client][:autostart],
     types: types, parents: parents,
     type: data[:type].to_s, parent: data[:parent].to_s
   }
